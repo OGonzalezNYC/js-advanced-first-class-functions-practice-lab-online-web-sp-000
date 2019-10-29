a266951e0a86114a4de01825c87bc1e2ea0e1ee1
Omar Gonzalez <ogonzaleznyc@gmail.com>
2019-10-29 05:51:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
 // Code your solution in this file!
 
-// function logDriverNames (array) {
-//   array.forEach(function (driver) {
-//     console.log(driver.name);
-//   })
-// }
-
 // function logDriversByHometown(array, location) {
 //   array.forEach(function(driver) {
 //     if (driver.hometown === location) {
