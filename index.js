63d177e369f35b4e282b8933a986aa4592f4efbc
Omar Gonzalez <ogonzaleznyc@gmail.com>
2019-10-29 05:41:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -117,8 +117,8 @@ function driversByName(driversArray) {
 
 
 function totalRevenue(driversArray) {
-  return driversArray.reduce(function(accumulator, currentValue) {
-    return accumulator + currentValue.revenue;
+  return driversArray.reduce(function(accumulator, driver) {
+    return accumulator + driver.revenue;
   }, 0);
 }
 
